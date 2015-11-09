6f4b3b5422f06633a37f57de024ede13e2a35eec
Jeff Meyer <jcmeyer10@gmail.com>
2015-11-09 16:18:24 -0500

    creates JS files for on click functions and JS source files to index.html

---
+++ startbootstrap-one-page-wonder-1.0.3/js/on_tap_api.js
@@ -0,0 +1,69 @@
+'use strict';
+var on_tap_api = {
+  url: 'http://localhost:3000',
+
+  ajax: function(config, cb) {
+    $.ajax(config).done(function(data, textStatus, jqxhr) {
+      cb(null, data);
+    }).fail(function(jqxhr, status, error) {
+      cb({jqxher: jqxhr, status: status, error: error});
+    });
+  },
+
+  register: function register(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/register',
+      contentType: 'application/json',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  login: function login(credentials, callback) {
+    this.ajax({
+      method: 'POST',
+      url: this.url + '/login',
+      contentType: 'application/json',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+}
+
+//   // Do this when the page loads
+//   console.log('page loaded');
+
+// var on_tap =
+//   $('#register').('submit', (function(event) {
+//    $.ajax({
+//       method: 'GET',
+//       url: 'http://localhost:3000/register'
+//       contentType: 'application/json; charset=utf-8',
+//       data: JSON.stringify(credentials),
+//       dataType: 'json'
+//     }, callback);
+//   });
+
+//   $('#login').on('submit', function(event){
+
+//     $.ajax({
+//       method: 'GET',
+//       url: 'http://localhost:3000/login'
+//       contentType: 'application/json; charset=utf-8',
+//       data: JSON.stringify(credentials),
+//       dataType: 'json'
+//     }, callback);
+
+//   });
+
+//   $('imaginary_container').on('submit', function(event){
+//     $.ajax({
+//       method: 'GET',
+//       url: 'http://localhost:3000/locations'
+//       contentType: 'application/json; charset=utf-8',
+//       data: JSON.stringify(credentials),
+//       dataType: 'json'
+//     }, callback);
+//     })
+//   });
---
+++ startbootstrap-one-page-wonder-1.0.3/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  var form2object = function(form) {
+    var data = {};
+    $(form).find('input').each(function(index, element) {
+      var type = $(this).attr('type');
+      if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+        data[$(this).attr('name')] = $(this).val();
+      }
+    });
+    return data;
+  };
+  var wrap = function wrap(root, formData) {
+    var wrapper = {};
+    wrapper[root] = formData;
+    return wrapper;
+  };
+
+  var callback = function callback(error, data) {
+    if (error) {
+      console.error(error);
+      $('#result').val('status: ' + error.status + ', error: ' +error.error);
+      return;
+    }
+    $('#result').val(JSON.stringify(data, null, 4));
+  };
+
+  $('#register').on('submit', function(e) {
+    var credentials = wrap('credentials', form2object(this));
+    on_tap_api.register(credentials, callback);
+    e.preventDefault();
+  });
+
+  $('#login').on('submit', function(e) {
+    var credentials = wrap('credentials', form2object(this));
+    var cb = function cb(error, data) {
+      if (error) {
+        callback(error);
+        return;
+      }
+      callback(null, data);
+      $('.token').val(data.user.token);
+      game.token = data.user.token;
+      $('.gameboard').show("slow");
+    };
+    e.preventDefault();
+    on_tap_api.login(credentials, cb);
+  });
+});
