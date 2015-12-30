ba0ec1234a7c304dd4fcb331d5606794bbf81480
Jeff Meyer <jcmeyer10@gmail.com>
2015-12-30 14:09:04 -0500

    changes ajax url to heroku

--- startbootstrap-one-page-wonder-1.0.3/js/on_tap_api.js
+++ startbootstrap-one-page-wonder-1.0.3/js/on_tap_api.js
@@ -1,5 +1,5 @@
 var on_tap_api = {
-  url: 'http://localhost:3000',
+  url: 'https://obscure-fjord-2393.herokuapp.com',
 
   ajax: function (config, cb) {
     $.ajax(config).done(function(data, textStatus, jqxhr) {
